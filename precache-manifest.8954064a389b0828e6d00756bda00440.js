3a43791dd966c191b316980b83f53778114aac77
Dazacher <dazacher2000@yahoo.com>
2020-07-21 15:02:37 -0700

    Updates

--- precache-manifest.8954064a389b0828e6d00756bda00440.js
+++ precache-manifest.8954064a389b0828e6d00756bda00440.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "615599d76d2bd988f4217db50ebdd59e",
+    "revision": "beec3091987d758b4652456edf26181a",
     "url": "/react-portfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "41e5461e39c5e9c8bcfd",
-    "url": "/react-portfolio/static/css/main.fb5e6770.chunk.css"
+    "revision": "cab890b61995dc6602df",
+    "url": "/react-portfolio/static/css/main.45625b76.chunk.css"
   },
   {
     "revision": "f1a9e1d295c0ed0aaf74",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/js/2.b5e16067.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "41e5461e39c5e9c8bcfd",
-    "url": "/react-portfolio/static/js/main.0eec6b0d.chunk.js"
+    "revision": "cab890b61995dc6602df",
+    "url": "/react-portfolio/static/js/main.a58404b2.chunk.js"
   },
   {
     "revision": "aff33cef9dbb8787d6b3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.0a8cda2309eaa0f79b015c194da113c0.js"
+  "/react-portfolio/precache-manifest.8954064a389b0828e6d00756bda00440.js"
 );
 
 self.addEventListener('message', (event) => {
