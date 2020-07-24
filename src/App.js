c5927e92f524b12ad14232f7188e9bbee948fddf
Dazacher <dazacher2000@yahoo.com>
2020-07-23 18:00:27 -0700

    Trying to get Name to link to about page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar"
 import Footer from "./components/footer/Footer"
 import About from "./components/pages/About";
