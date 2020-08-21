11ee46caee11b236f155d4ef95f57dc852e90cf3
dazacher <dazacher2000@yahoo.com>
2020-08-20 17:16:48 -0700

    Added AOS to About page.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,40 +1,76 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Email from "../../assets/images/email-32.png"
-import GitHub from "../../assets/images/github-9-32.png"
-import Pdf from "../../assets/images/document-32.png"
-import LinkedIn from "../../assets/images/linkedin-3-32.png"
+import Email from "../../assets/images/email-32.png";
+import GitHub from "../../assets/images/github-9-32.png";
+import Pdf from "../../assets/images/document-32.png";
+import LinkedIn from "../../assets/images/linkedin-3-32.png";
 import "./Footer.css";
 import Container from "react-bootstrap/esm/Container";
 
 const Footer = () => {
-    return (
-        <Container className="contactInfo" fluid>
-            <Row>
-                <Col xs={12}>
-                    <h4>Contact Me! I look forward to working with you.</h4>
-                </Col>
-            </Row>
-            <Row className="text-center"> 
-                <Col xs={12}>
-                    {/* <!-- Link to my email address so someone can email me. --> */}
-                    <a href="mailto:dazacher2000@yahoo.com? subject=subject text" id="emailImage">
-                        <img src={Email} className="indexImages" alt="email" id="email" /></a>
-                    {/* <!-- Link to my GitHub page --> */}
-                    <a href="https://github.com/dazacher" target="_blank" rel="noopener noreferrer" id="githubImage">
-                        <img src={GitHub} className="indexImages" alt="Github" id="github" /></a>
-                    {/* <!-- Link to my Resume page --> */}
-                    <a href={(require("../../assets/images/Dee_Ann_Scanniello_Resume_Web_Developer_Full_Stack.pdf"))} id="fileImage">
-                        <img src={Pdf} className="indexImages" alt="Resume" id="file" />
-                    </a>
-                    {/* <!-- Link to my LinkedIn page --> */}
-                    <a href="https://linkedin.com/in/deeannscanniello" target="_blank" rel="noopener noreferrer" id="linkedinImage">
-                        <img src={LinkedIn} className="indexImages" alt="LinkedIn" id="linkedin" /></a>
-                </Col>
-            </Row>
-        </Container>
-    );
+  return (
+    <Container className="contactInfo" fluid>
+      <Row>
+        <Col xs={12}>
+          <h4>Contact Me! I look forward to working with you.</h4>
+        </Col>
+      </Row>
+      <Row className="text-center">
+        <Col xs={12} className="textCenter">
+          {/* <!-- Link to my email address so someone can email me. --> */}
+          <div
+            data-aos="flip-down"
+            data-aos-offset="200"
+            data-aos-easing="ease-in-sine"
+            data-aos-duration="1200"
+          >
+            <a
+              href="mailto:dazacher2000@yahoo.com? subject=subject text"
+              id="emailImage"
+            >
+              <img src={Email} className="indexImages" alt="email" id="email" />
+            </a>
+            {/* <!-- Link to my GitHub page --> */}
+            <a
+              href="https://github.com/dazacher"
+              target="_blank"
+              rel="noopener noreferrer"
+              id="githubImage"
+            >
+              <img
+                src={GitHub}
+                className="indexImages"
+                alt="Github"
+                id="github"
+              />
+            </a>
+            {/* <!-- Link to my Resume page --> */}
+            <a
+              href={require("../../assets/images/Dee_Ann_Scanniello_Resume_Web_Developer_Full_Stack2.pdf")}
+              id="fileImage"
+            >
+              <img src={Pdf} className="indexImages" alt="Resume" id="file" />
+            </a>
+            {/* <!-- Link to my LinkedIn page --> */}
+            <a
+              href="https://linkedin.com/in/deeannscanniello"
+              target="_blank"
+              rel="noopener noreferrer"
+              id="linkedinImage"
+            >
+              <img
+                src={LinkedIn}
+                className="indexImages"
+                alt="LinkedIn"
+                id="linkedin"
+              />
+            </a>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Footer;
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,40 +1,77 @@
 import React from "react";
-// import HTMLAnchorElement from "react";
-// import Me from "../../../assets/images/Me.jpeg";
-
 import "./About.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
-function About() {
-    return (
-        <Container id="index">
-            <Row>
-                <Col xs={12} md={3} className="img-fix">
-                    {/* <!-- Picture of myself and a couple of paragraphs about myself--> */}
-                    <Image fluid className="img-responsive mx-auto" id="deeannImg" src={process.env.PUBLIC_URL + "/Me.jpeg"} roundedCircle alt="Dee Ann Scanniello" />
-                </Col>
-                <Col xs={12} md={9} id="dafs">
-                    <h1>I am Dee&nbsp;Ann...</h1>
-                    <p id="fullstack">a full stack web developer</p>
-                </Col>
-            </Row>
-            <Row>
-                <Col-12>
-                    <h2>About Me</h2>
-                </Col-12>
-            </Row>
-            <Row>
-                <Col>
-                    {/* <!-- Picture of myself and a couple of paragraphs about myself--> */}
-                    <p>
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 
-                        My name is Dee Ann Scanniello. I am a Full Stack Web Developer, self-driven, easily adaptable and flexible, with a knack for attention to detail. I have a passion for full stack development. Quick learner, who enjoys challenges and resolving problems. A graduate from the University of Arizona Bootcamp with a Certificate in Full Stack Web Development and a Bachelor’s degree from DeVry University in Computer Information Systems. Skilled in JavaScript, jQuery, Bootstrap, AJAX, Git Bash, Sequealize, Node.js, Express.js, Inquirer, React.js (this Portfolio was created with React.js), MySql2, Git Hub, Travis CI/CD, Handlebars.js, OOP, Heroku, RESTful API's and Visual Studio. Independent, organized, motivated, detail-oriented, and have a special skill at final testing software. Looking to grow as a programmer and eventually become a mentor to others.</p>
-                </Col>
-            </Row>
-        </Container>
-    );
-};
+function About() {
+  return (
+    <Container id="index">
+      <Row>
+        <Col xs={12} md={3} className="img-fix">
+          {/* <!-- Picture of myself and a couple of paragraphs about myself--> */}
+          <div
+            data-aos="zoom-in-left"
+            data-aos-offset="200"
+            data-aos-easing="ease-in-sine"
+            data-aos-duration="1600"
+          >
+            <Image
+              fluid
+              className="img-responsive mx-auto"
+              id="deeannImg"
+              src={process.env.PUBLIC_URL + "/Me.jpeg"}
+              roundedCircle
+              alt="Dee Ann Scanniello"
+            />
+          </div>
+        </Col>
+        <Col xs={12} md={9} id="dafs">
+          <div
+            data-aos="fade-up-left"
+            data-aos-offset="200"
+            data-aos-easing="ease-in-sine"
+            data-aos-duration="1600"
+          >
+            <h1>I am Dee&nbsp;Ann...</h1>
+            <p id="fullstack">a full stack web developer</p>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col-12>
+          <h2>About Me</h2>
+        </Col-12>
+      </Row>
+      <Row>
+        <Col>
+          {/* <!-- Picture of myself and a couple of paragraphs about myself--> */}
+          <p>
+            My name is Dee Ann Scanniello. I am a Full Stack Web Developer,
+            self-driven, easily adaptable and flexible, with a knack for
+            attention to detail. I have a passion for full stack development.
+            Quick learner, who enjoys challenges and resolving problems. A
+            graduate from the University of Arizona Bootcamp with a Certificate
+            in Full Stack Web Development and a Bachelor’s degree from DeVry
+            University in Computer Information Systems. Skilled in JavaScript,
+            jQuery, Bootstrap, AJAX, Git Bash, Sequealize, Node.js, Express.js,
+            Inquirer, React.js (this Portfolio was created with React.js),
+            MySql2, Git Hub, Travis CI/CD, Handlebars.js, OOP, Heroku, RESTful
+            API's, AOS, Moment, Material Design, Axios, MongoDB, Mongoose, and
+            Visual Studio. Proven soft-skills such as independent, organized,
+            motivated, collaboration, team work, conflict resolution, problem
+            solving, detail-oriented, and have a special skill at final testing
+            software. Looking to grow as a programmer and eventually become a
+            mentor to others.
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 export default About;
